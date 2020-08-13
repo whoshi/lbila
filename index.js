3ad9c330b14f404b0882435022cddd4694b831d3
whoshi <whoshi@v004.vaio.ne.jp>
2020-08-13 22:49:36 +0900

    Update index.js

--- index.js
+++ index.js
@@ -19,15 +19,16 @@ $(function () {
 
     // 送信
     $('form').submit(function () {
-        var date = $('input[name="date"]').val();
-        var number = $('input[name="number"]:checked').val();
-        var names = '';
-        $('#form-name').children().each(function (i, elm) {
-            names += $(elm).val() + '、';
-        })
-        names = names.slice(0, -1);
+        //var date = $('input[name="date"]').val();
+        //var number = $('input[name="number"]:checked').val();
+        //var names = '';
+        //$('#form-name').children().each(function (i, elm) {
+        //    names += $(elm).val() + '、';
+        //})
+        //names = names.slice(0, -1);
 
-        var msg = `希望日：${date}\n人数：${number}\n氏名：${names}`;
+        //var msg = `希望日：${date}\n人数：${number}\n氏名：${names}`;
+        var msg = 'メモを登録しました!
         sendText(msg);
 
         return false;
