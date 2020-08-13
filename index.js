3d962e3afd89eec59b5c5a8fbfd02bde9e3bde34
whoshi <whoshi@v004.vaio.ne.jp>
2020-08-13 22:56:39 +0900

    Update index.js

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ $(function () {
         //names = names.slice(0, -1);
 
         //var msg = `希望日：${date}\n人数：${number}\n氏名：${names}`;
-        var msg = "メモを登録しました!"
+        var msg = 'メモを登録しました!';
         sendText(msg);
 
         return false;
