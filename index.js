55ddc8a8f114480bc1fee02eed3d86689bb75f49
whoshi <whoshi@v004.vaio.ne.jp>
2020-08-14 21:29:42 +0900

    Update index.js

--- index.js
+++ index.js
@@ -25,17 +25,33 @@ $(function () {
       var inputdata = "登録しました" + "\n" + "【顧客名】" + "\n" + customername + "\n" + "【ヒヤリング内容】" + "\n" + hearing + "\n";
       inputdata = inputdata + "【メモ】" + "\n"　+ memo + "\n" + "【メモ分類】" + "\n" + category;
         
-      //$.post( 'https://script.google.com/macros/s/AKfycbw6elowD1ut9p7iUxwcG9i8ov3ONKYCMeQ4mjei7ZsPytppZrmr/exec', category) )
+      var json_asocc = 
+        [
+            {
+                'customername': customername,
+                'hearing': hearing,
+                'memo': memo
+                'category': category
+            }
+        ];
+     
+    //JSONにエンコード
+    var json_text = JSON.stringify(json_asocc);
  
-        //サーバーからの返信を受け取る
-        //.done( function(data) {  } )
- 
-        //通信エラーの場合
-        //.fail( function() {  } )
- 
-       //通信が終了した場合
-       //always ( function() {  } )
+    //データを送信
+    xhr = new XMLHttpRequest;       //インスタンス作成
+    xhr.onload = function(){        //レスポンスを受け取った時の処理（非同期）
+        var res = xhr.responseText;
+        if (res.length>0) alert(res);
+    };
+    xhr.onerror = function(){       //エラーが起きた時の処理（非同期）
+        alert("error!");
+    }
+    xhr.open('post', "api_sendjson.php", true);    //(1)
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.send(json_text);    //送信実行
         
+      
         
       sendText(inputdata);//To LINE 送信
 
