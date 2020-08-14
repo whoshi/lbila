79c75e6b4136e4e44a01fbc513b600dd6ed71a98
whoshi <whoshi@v004.vaio.ne.jp>
2020-08-14 20:43:19 +0900

    Update index.js

--- index.js
+++ index.js
@@ -25,16 +25,16 @@ $(function () {
       var inputdata = "登録しました" + "\n" + "【顧客名】" + "\n" + customername + "\n" + "【ヒヤリング内容】" + "\n" + hearing + "\n";
       inputdata = inputdata + "【メモ】" + "\n"　+ memo + "\n" + "【メモ分類】" + "\n" + category;
         
-      $.post( 'https://script.google.com/macros/s/AKfycbw6elowD1ut9p7iUxwcG9i8ov3ONKYCMeQ4mjei7ZsPytppZrmr/exec', category) )
+      //$.post( 'https://script.google.com/macros/s/AKfycbw6elowD1ut9p7iUxwcG9i8ov3ONKYCMeQ4mjei7ZsPytppZrmr/exec', category) )
  
         //サーバーからの返信を受け取る
-        .done( function(data) {  } )
+        //.done( function(data) {  } )
  
         //通信エラーの場合
-        .fail( function() {  } )
+        //.fail( function() {  } )
  
        //通信が終了した場合
-       always ( function() {  } )
+       //always ( function() {  } )
         
         
       sendText(inputdata);//To LINE 送信
