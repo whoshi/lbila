c15085541d61f9e81f9c964440a56f82e930939f
whoshi <whoshi@v004.vaio.ne.jp>
2020-08-14 17:08:13 +0900

    Update index.js

--- index.js
+++ index.js
@@ -34,43 +34,14 @@ $(function () {
       var inputdata = "登録しました" + "\n" + "【顧客名】" + "\n" + customername + "\n" + "【ヒヤリング内容】" + "\n" + hearing + "\n";
       inputdata = inputdata + "【メモ】" + "\n"　+ memo + "\n" + "【メモ分類】" + "\n" + category;
         
-        //sendText('登録しました');
-        data = {
-        gassenddata:"gassenddata"
-        customername: customername,
-        hearing: hearing,
-        memo: memo,
-        category: category,
-        }
         
         
-        //var gassenddata = "gassenddata" + "," + customername + "," + hearing + "," + memo + "," + category
-        //gassend(data);//Gasへデータ引き渡し
-        
-        sendText(inputdata);//To LINE 送信
+      sendText(inputdata);//To LINE 送信
         
         
 
         return false;
     });
     
-    //
-    function gassend(value) {
- 
-    var form = document.createElement('form');
-    var request = document.createElement('input');
- 
-    form.method = 'POST';
-    form.action = 'https://script.google.com/macros/s/AKfycbyYeIad_7xrWIA5QBA9aLj9xPF6_WMgPIDAErHdw6rPea1ZuAa5/exec';
- 
-    request.type = 'hidden'; //入力フォームが表示されないように
-    request.name = 'text';
-    request.value = value;
- 
-    form.appendChild(request);
-    document.body.appendChild(form);
- 
-    form.submit();
- 
-}
+    
 });
