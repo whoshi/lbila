eb678d13d68399aa0f2f11ce78a5b2fcc4c964b9
whoshi <whoshi@v004.vaio.ne.jp>
2020-08-14 23:15:09 +0900

    Update index.js

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ $(function () {
                 };
 
             alert(JSON.stringify(JSONdata));
-
+/*
             $.ajax({
                 type : 'post',
                 url : url,
@@ -55,6 +55,7 @@ $(function () {
                     alert(JSON.stringify(data));
                     
                 }
+                */
       /*
       var json_asocc = 
         [
