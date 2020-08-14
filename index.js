bd66cdb029cb21acdbb3974ee1fcdd9dca7ca0f2
whoshi <whoshi@v004.vaio.ne.jp>
2020-08-14 23:58:29 +0900

    Update index.js

--- index.js
+++ index.js
@@ -36,18 +36,28 @@ $(function () {
     //JSONにエンコード
     var json_text = JSON.stringify(JSONdata);
  
-    //データを送信
-    xhr = new XMLHttpRequest;       //インスタンス作成
-    xhr.onload = function(){        //レスポンスを受け取った時の処理（非同期）
-        var res = xhr.responseText;
-        if (res.length>0) alert(res);
-    };
-    xhr.onerror = function(){       //エラーが起きた時の処理（非同期）
-        //alert("error!");
+    var xmlHttpRequest = new XMLHttpRequest();
+    xmlHttpRequest.onreadystatechange = function()
+    {
+        var READYSTATE_COMPLETED = 4;
+        var HTTP_STATUS_OK = 200;
+ 
+        if( this.readyState == READYSTATE_COMPLETED
+         && this.status == HTTP_STATUS_OK )
+        {
+            // レスポンスの表示
+            alert( this.responseText );
+            
+        }
     }
-    xhr.open('post', url, true);    //(1)
-    xhr.setRequestHeader('Content-Type', 'application/json');
-    xhr.send(json_text);    //送信実行
+ 
+    xmlHttpRequest.open( 'POST', 'https://script.google.com/macros/s/AKfycbw6elowD1ut9p7iUxwcG9i8ov3ONKYCMeQ4mjei7ZsPytppZrmr/exec',false );
+ 
+    // サーバに対して解析方法を指定する
+    xmlHttpRequest.setRequestHeader( 'Content-Type', 'application/json');
+ 
+    // データをリクエスト ボディに含めて送信する
+    xmlHttpRequest.send( JSON.stringify(JSONdata) );
             
      
       sendText(inputdata);//To LINE 送信
