571b79ae218a80fd1d13f39c8bbe7152a2da17c7
whoshi <whoshi@v004.vaio.ne.jp>
2020-08-14 20:21:57 +0900

    Update index.js

--- index.js
+++ index.js
@@ -20,15 +20,15 @@ $(function () {
               }
           }
       }
-      var postdata = customername;
+      //var postdata = customername;
       //postdata = postdata + ",";
 
       
-      gaspost(postdata);
+      
         
       var inputdata = "登録しました" + "\n" + "【顧客名】" + "\n" + customername + "\n" + "【ヒヤリング内容】" + "\n" + hearing + "\n";
       inputdata = inputdata + "【メモ】" + "\n"　+ memo + "\n" + "【メモ分類】" + "\n" + category;
-        
+        gaspost(inputdata);
       
         sendText(inputdata);//To LINE 送信
 
