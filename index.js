3c2016e5d8f1829acd5fa84153d7691340404d47
whoshi <whoshi@v004.vaio.ne.jp>
2020-08-14 19:49:44 +0900

    Create index.js

---
+++ index.js
@@ -0,0 +1,34 @@
+$(function () {
+    
+    // 送信
+    $('form').submit(function () {
+      var customername = document.getElementById("input_customername").value;
+      var hearing = document.getElementById("input_hearing").value;
+      var memo = document.getElementById("input_memo").value;
+    
+      var element = document.getElementById( "myForm" ) ;
+      var radioNodeList = element.category ;
+      var category = radioNodeList.value ;
+        
+      if(customername === ''){
+          if(hearing === ''){
+              if(memo === ''){
+                  if(category === ''){
+                      alert('メモ入力が何もありません');
+                      return false;
+                  }
+              }
+          }
+      }
+      
+
+      var inputdata = "登録しました" + "\n" + "【顧客名】" + "\n" + customername + "\n" + "【ヒヤリング内容】" + "\n" + hearing + "\n";
+      inputdata = inputdata + "【メモ】" + "\n"　+ memo + "\n" + "【メモ分類】" + "\n" + category;
+        
+      
+        
+      sendText(inputdata);//To LINE 送信
+
+        return false;
+    });
+});
