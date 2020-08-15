0aa58ef27d2c13683f46ce01c8b4ac84a816cdb6
whoshi <whoshi@v004.vaio.ne.jp>
2020-08-15 14:51:39 +0900

    Update liff.js

--- liff.js
+++ liff.js
@@ -60,30 +60,6 @@ function getdisplayname(){
     
     return msg;
 }
-// setIntervalを使う方法
-function mysleep(waitSec, callbackFunc) {
- 
-    // 経過時間（秒）
-    var spanedSec = 0;
- 
-    // 1秒間隔で無名関数を実行
-    var id = setInterval(function () {
- 
-        spanedSec++;
- 
-        // 経過時間 >= 待機時間の場合、待機終了。
-        if (spanedSec >= waitSec) {
- 
-            // タイマー停止
-            clearInterval(id);
- 
-            // 完了時、コールバック関数を実行
-            if (callbackFunc) callbackFunc();
-        }
-    }, 1000);
- 
-}
-
 
 // Webブラウザからメッセージ送信
 function shareTargetPicker(text) {
