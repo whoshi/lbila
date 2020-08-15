2065a3f90b93f21dde777ea08f663aba7480a9c5
whoshi <whoshi@v004.vaio.ne.jp>
2020-08-15 11:25:55 +0900

    Update index.js

--- index.js
+++ index.js
@@ -25,18 +25,28 @@ $(function () {
       var inputdata = "登録しました" + "\n" + "【顧客名】" + "\n" + customername + "\n" + "【ヒヤリング内容】" + "\n" + hearing + "\n";
       inputdata = inputdata + "【メモ】" + "\n"　+ memo + "\n" + "【メモ分類】" + "\n" + category;
       
+      //postより先にsendtextしてGASスプレッドシートにLINE表示名を設定する。  
       sendText(inputdata);//To LINE 送信
+      
+      const datecurrent = new Date();
+	  const nowdate = datecurrent.getFullYear() + "年" + 
+				(datecurrent.getMonth() + 1)  + "月" + 
+				datecurrent.getDate() + "日" + 
+				datecurrent.getHours() + "時" + 
+				datecurrent.getMinutes() + "分" + 
+				datecurrent.getSeconds() + "秒";
         
       var url = 'https://script.google.com/macros/s/AKfycbw6elowD1ut9p7iUxwcG9i8ov3ONKYCMeQ4mjei7ZsPytppZrmr/exec';
 
       var JSONdata = {
+                    nowdate:nowdate
                     customername: customername,
                     hearing: hearing,
                     memo: memo ,
                     category: category 
        };
 
-     alert(JSON.stringify(JSONdata));
+     //alert(JSON.stringify(JSONdata));
      // {"cd":"100", "name":"Taro"},
      $.post(url,
       JSONdata,
