24f8660778246f6a0553c5d480421eefd0e3345a
whoshi <whoshi@v004.vaio.ne.jp>
2020-08-15 17:22:39 +0900

    Update liff.js

--- liff.js
+++ liff.js
@@ -38,7 +38,7 @@ function sendMessages(text) {
         'type': 'text',
         'text': text
     }]).then(function () {
-        //liff.closeWindow();
+        liff.closeWindow();
     }).catch(function (error) {
         window.alert('Failed to send message ' + error);
     });
