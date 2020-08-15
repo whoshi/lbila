2b58bbcfff90f98d5ecbe175261d20db5892d225
whoshi <whoshi@v004.vaio.ne.jp>
2020-08-15 14:46:54 +0900

    Update index.js

--- index.js
+++ index.js
@@ -41,7 +41,13 @@ $(function () {
 	    
       getprofile();
 	    
+      sleep(3, function () {
+ 
       alert(getdisplayname());
+ 
+      });
+	    
+      //alert(getdisplayname());
 
       var JSONdata = {
                     nowdate:nowdate,
@@ -65,4 +71,30 @@ $(function () {
       
         return false;
     });
+   // setIntervalを使う方法
+function sleep(waitSec, callbackFunc) {
+ 
+    // 経過時間（秒）
+    var spanedSec = 0;
+ 
+    // 1秒間隔で無名関数を実行
+    var id = setInterval(function () {
+ 
+        spanedSec++;
+ 
+        // 経過時間 >= 待機時間の場合、待機終了。
+        if (spanedSec >= waitSec) {
+ 
+            // タイマー停止
+            clearInterval(id);
+ 
+            // 完了時、コールバック関数を実行
+            if (callbackFunc) callbackFunc();
+        }
+    }, 1000);
+ 
+}
+ 
+ 
+
 });
