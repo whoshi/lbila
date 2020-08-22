1ca640461298c1a7d59b2a7a1988e95fc5c42271
whoshi <whoshi@v004.vaio.ne.jp>
2020-08-22 18:33:17 +0900

    Update index.js

--- index.js
+++ index.js
@@ -59,13 +59,9 @@ $(function () {
       JSONdata,
       function(dt){
          //console.log(dt);liff.closeWindow();
-	 if(dt.message === 'ロック中'){
-		 window.alert("他の人がアクセス中です、少し待って再実行して下さい！);
-	 }else{
-		liff.closeWindow(); 
-		 
-	 }
-         
+	 
+         window.alert(dt.message);
+	     liff.closeWindow(); 
 	 
      }
      );
