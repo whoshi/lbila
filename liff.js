bf7ef990f5ab10af4e9560968393e495a1b4b58e
whoshi <whoshi@v004.vaio.ne.jp>
2020-09-14 19:39:07 +0900

    Update liff.js

--- liff.js
+++ liff.js
@@ -104,3 +104,25 @@ function shareTargetPicker(text) {
         window.alert('Failed to send message ' + error);
     });
 }
+/* ------------------------------
+ 表示用の関数
+ ------------------------------ */
+function dispLoading(msg){
+  // 引数なしの場合、メッセージは非表示。
+  if(msg === undefined ) msg = "";
+  
+  // 画面表示メッセージを埋め込み
+  var innerMsg = "<div id='innerMsg'>" + msg + "</div>";  
+  
+  // ローディング画像が非表示かどうかチェックし、非表示の場合のみ出力。
+  if($("#nowLoading").length == 0){
+    $("body").append("<div id='nowLoading'>" + innerMsg + "</div>");
+  }
+}
+ 
+/* ------------------------------
+ 表示ストップ用の関数
+ ------------------------------ */
+function removeLoading(){
+  $("#nowLoading").remove();
+}  
