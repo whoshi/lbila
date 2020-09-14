13ecd3a8981b2c5e21f87be30c6af9fb8bb1aaba
whoshi <whoshi@v004.vaio.ne.jp>
2020-09-14 19:38:20 +0900

    Update index.js

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ $(function () {
       inputdata = inputdata + "【メモ】" + "\n"　+ memo + "\n" + "【メモ分類】" + "\n" + category;
       
       //postより先にsendtextしてGASスプレッドシートにLINE表示名を設定する。  
-      sendText(inputdata);//To LINE 送信
+      //sendText(inputdata);//To LINE 送信
       
       const datecurrent = new Date();
       const nowdate = datecurrent.getFullYear() + "年" + 
@@ -36,7 +36,7 @@ $(function () {
 				datecurrent.getMinutes() + "分" + 
 				datecurrent.getSeconds() + "秒";
         
-      var url = 'https://script.google.com/macros/s/AKfycbw6elowD1ut9p7iUxwcG9i8ov3ONKYCMeQ4mjei7ZsPytppZrmr/exec';
+      //var url = 'https://script.google.com/macros/s/AKfycbw6elowD1ut9p7iUxwcG9i8ov3ONKYCMeQ4mjei7ZsPytppZrmr/exec';
       
       //var dispname = document.getElementById('id_linedisp');
       //alert(dispname);
@@ -55,12 +55,22 @@ $(function () {
 
      //alert(JSON.stringify(JSONdata));
      // {"cd":"100", "name":"Taro"},
+	      
+     //スピナー表示
+     //インジケータ表示
+     // Loading 画像を表示
+     dispLoading("データ暗号化処理中...");
+	      
      $.post(url,
       JSONdata,
       function(dt){
          //console.log(dt);liff.closeWindow();
+	          //インジケータ除去
+	          // Loading 画像を消す
+                  removeLoading();
 	          if(dt.message == 'success!')
 		  {
+			  sendText(inputdata);//To LINE 送信
 			  liff.closeWindow(); 
 		  }else
 		  {
@@ -82,7 +92,6 @@ $(function () {
                     memo: memo ,
                     category: category 
        };
-
      //alert(JSON.stringify(JSONdata));
      // {"cd":"100", "name":"Taro"},
      $.post(url,
